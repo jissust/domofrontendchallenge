f00094338f33432a0213bc558cf65dcb963e3ce5
jesustissera <jesustissera@gmail.com>
2024-05-01 04:57:15 -0300

    completo servicios y termino tecnologias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OurServicesComponent } from './pages/our-services/our-services.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, OurServicesComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, OurServicesComponent, TechnologyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
