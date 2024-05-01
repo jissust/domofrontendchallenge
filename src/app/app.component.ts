40d795379ccca84e83b59e9f3c1f2e95e2c88bad
jesustissera <jesustissera@gmail.com>
2024-05-01 16:26:56 -0300

    completo nuestros servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OurServicesComponent } from './pages/our-services/our-services.component';
 import { TechnologyComponent } from './pages/technology/technology.component';
+import { CaseStudiesComponent } from './pages/case-studies/case-studies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, OurServicesComponent, TechnologyComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, OurServicesComponent, TechnologyComponent, CaseStudiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/case-studies/case-studies.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-studies',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './case-studies.component.html',
+  styleUrl: './case-studies.component.css',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CaseStudiesComponent {
+  
+}
